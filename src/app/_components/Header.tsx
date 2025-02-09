b6ce2a1bf0c21af6c9029eafe38321fc19c947fd
eotkd4791 <eotkd4791@gmail.com>
2025-02-10 00:27:39 +0900

    로고 추가 및 색상 변경

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,15 +1,19 @@
-import { Box, Container, Text, Heading, Separator } from '@radix-ui/themes';
+import { Box, Container, Text, Heading, Separator, Flex } from '@radix-ui/themes';
+import { Logo } from '@/app/_logo/Logo';
 
 export function Header() {
 	return (
 		<Box asChild>
 			<header>
-				<Container size="3" p="3">
-					<Text size="5">
-						<Heading as="h1" color="crimson">
-							Share Me
-						</Heading>
-					</Text>
+				<Container size="2" p="3">
+					<Flex align="center" gap="2">
+						<Logo />
+						<Text size="5">
+							<Heading as="h1" color="cyan">
+								Share Me
+							</Heading>
+						</Text>
+					</Flex>
 				</Container>
 				<Separator orientation="horizontal" size="4" />
 			</header>
