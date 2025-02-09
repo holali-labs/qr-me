66844c6acfafa99ceca75d558ffe067853287cf2
eotkd4791 <eotkd4791@gmail.com>
2025-02-10 00:27:55 +0900

    스타일 추가

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -2,7 +2,7 @@ import { Box, Container, Text } from '@radix-ui/themes';
 
 export function Footer() {
 	return (
-		<Box asChild style={{ backgroundColor: 'var(--gray-a4)' }}>
+		<Box asChild style={{ marginTop: 'auto', backgroundColor: 'var(--gray-a4)' }}>
 			<footer>
 				<Container size="3" p="3">
 					<Text size="3">ShareMe &copy; {new Date().getFullYear()} Daesang Stefan You.</Text>
