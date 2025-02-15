aab5678cec12e8be960d65ebaf89f4981875a7a6
eotkd4791 <eotkd4791@gmail.com>
2025-02-15 20:58:06 +0900

    서비스명 변경

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
 		<Box asChild style={{ marginTop: 'auto', backgroundColor: 'var(--gray-a4)' }}>
 			<footer>
 				<Container size="2" p="3">
-					<Text size="3">ShareMe &copy; {new Date().getFullYear()} Daesang Stefan You.</Text>
+					<Text size="3">QR Me &copy; {new Date().getFullYear()} Daesang Stefan You.</Text>
 				</Container>
 			</footer>
 		</Box>
