5db5a4989a909a28b71dcadf0a3b05e24ecb5278
eotkd4791 <eotkd4791@gmail.com>
2025-02-15 21:08:07 +0900

    빌드 에러 수정 및 미리보기 컴포넌트 구조 수정

--- src/app/_components/BusinessCard/View.tsx
+++ src/app/_components/BusinessCard/View.tsx
@@ -1,6 +1,18 @@
-import type { ReactNode } from 'react';
-import { BusinessCard, type BusinessCardData } from '@/app/_components/BusinessCard';
+'use client';
 
-export function BusinessCardView(businessCardData: BusinessCardData & { qrcode: ReactNode }) {
-	return <BusinessCard {...businessCardData} />;
+import { BusinessCard } from '@/app/_components/BusinessCard';
+import { Container } from '@radix-ui/themes';
+import { useSearchParams } from 'next/navigation';
+import qs from 'query-string';
+import { EditorOutput } from '@/app/_components/BusinessCard/Editor/Output';
+
+export function BusinessCardView() {
+	const searchParams = useSearchParams();
+	const businessCardData = qs.parse(searchParams.toString());
+
+	return (
+		<Container m="8" maxWidth="450px">
+			<BusinessCard {...businessCardData} qrcode={<EditorOutput />} />
+		</Container>
+	);
 }
--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -1,18 +1,10 @@
-'use client';
-
-import { useSearchParams } from 'next/navigation';
-import { Container } from '@radix-ui/themes';
-import qs from 'query-string';
 import { BusinessCardView } from '@/app/_components/BusinessCard/View';
-import { EditorOutput } from '@/app/_components/BusinessCard/Editor/Output';
+import { Suspense } from 'react';
 
 export default function ViewPage() {
-	const searchParams = useSearchParams();
-	const businessCardData = qs.parse(searchParams.toString());
-
 	return (
-		<Container m="8" maxWidth="450px">
-			<BusinessCardView {...businessCardData} qrcode={<EditorOutput />} />
-		</Container>
+		<Suspense>
+			<BusinessCardView />
+		</Suspense>
 	);
 }
