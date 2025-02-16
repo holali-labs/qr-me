29c0e87ce3c2b98402aa395206e19ad85001b044
eotkd4791 <eotkd4791@gmail.com>
2025-02-16 15:02:25 +0900

    refactor: 공통 컴포넌트 layout에서 관리하도록 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Theme } from '@radix-ui/themes';
+import { Header } from '@/app/_components/Header';
+import { Footer } from '@/app/_components/Footer';
 
 import '@/app/globals.css';
 import '@radix-ui/themes/styles.css';
@@ -29,7 +31,9 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
 				<Theme accentColor="cyan" grayColor="sand" radius="large" scaling="95%">
+					<Header />
 					{children}
+					<Footer />
 				</Theme>
 			</body>
 		</html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import { Banner } from '@/app/_components/Banner';
 import { Editor } from '@/app/_components/BusinessCard/Editor';
-import { Footer } from '@/app/_components/Footer';
-import { Header } from '@/app/_components/Header';
 
 export default function Home() {
 	return (
 		<>
-			<Header />
 			<Banner />
 			<Editor />
-			<Footer />
 		</>
 	);
 }
