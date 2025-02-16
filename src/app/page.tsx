88316f321997180a3a0acb0af002f1c6341e004b
eotkd4791 <eotkd4791@gmail.com>
2025-02-16 23:55:04 +0900

    feat: metadata 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
 import { Banner } from '@/app/_components/Banner';
 import { Editor } from '@/app/_components/BusinessCard/Editor';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'QR Me',
+	description: 'Share your business card with ease.',
+	openGraph: {
+		title: 'QR Me',
+		description: 'Share your business card with ease.',
+		url: '/',
+		siteName: 'QR Me',
+		images: [
+			{
+				url: '/og.png',
+				width: 800,
+				height: 600,
+			},
+		],
+	},
+	twitter: {
+		title: 'QR Me',
+		description: 'Share your business card with ease.',
+		images: [
+			{
+				url: '/og.png',
+				width: 800,
+				height: 600,
+			},
+		],
+	},
+};
 
 export default function Home() {
 	return (
